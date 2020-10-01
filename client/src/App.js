e101dab965cf3d6b3e657aef60f8f338de35b991
soonamback <soonam.yi123@gmail.com>
2020-10-01 14:50:50 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import ContactInfo from "./components/ContactInfo";
+import ImageCar from "./components/ImageCar";
+import IntroApp from "./components/IntroApp";
+import SearchBar from "./components/SearchBar";
 
-function App() {
-	return <div className='App'></div>;
+class App extends Component {
+	render() {
+		let links = [
+			{ label: "Home", link: "#home", active: true },
+			{ label: "SignUp", link: "#signup" },
+			{ label: "LogIn", link: "#login" },
+		];
+
+		return (
+			<div className='container center'>
+				<NavBar links={links} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function NavBar() {
-	return <div></div>;
+class NavBar extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      showForm:false
+    };
+
+  }
+
+  showForm() {
+    this.setState({
+      showForm: !this.state.showForm
+    });
+  }
+render(){
+  let searchForm = this.state.Show ? (
+    <form className="search-form" method="POST">
+<input className="search-input" placeholder="Type and hit Enter" />
+
+
+
+    </form>
+  ) : '';
+
+  let signUp = this.props.sign.map ((sign, index) => {
+    let signUp = sign.active ? (
+      <a className="signin" href={sign.link>{sing.link}</a>
+    )
+  })
+  
+
+
 }
+
+
+
+}
+
+export default Navbar;
