a4562d99b4d20a9f7a6903dfd2b35a429e4991b0
Habid B Espinosa <hab.bad@hotmail.com>
2020-10-02 10:58:24 +0200

    authentication finish

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,44 @@
-import React, { Component } from "react";
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Navbar as Nav } from 'react-bootstrap';
+import { logout } from '../services/auth';
 
-export default class NavBar extends Component {
-	render() {
-		return (
-			<div>
-				<p>Welcome to the Nav Component!</p>
-			</div>
-		);
-	}
+
+const handleLogout = props => {
+  logout().then(() => {
+    props.setUser(null);
+  })
+}
+
+export default function Navbar(props) {
+  return (
+    <Nav className='nav justify-content-end' bg='primary'>
+			<Nav.Brand><h2>AstroShare</h2></Nav.Brand> 
+			<Nav.Toggle aria-controls="responsive-navbar-nav" />
+			<Nav.Collapse id="responsive-navbar-nav"></Nav.Collapse>
+			<Nav className="mr-auto">
+      {props.user && <Nav.Brand>Welcome {props.user.username} 🍺</Nav.Brand>}
+      <Nav.Brand>
+        <Link to='/'>Home</Link>
+      </Nav.Brand>
+      {props.user ? (
+        <>
+          <Nav.Brand>
+            <Link to='/' onClick={() => handleLogout(props)}>Logout</Link>
+          </Nav.Brand>
+        </>
+      ) : (
+          <>
+            <Nav.Brand>
+              <Link to='/signup'>Signup</Link>
+            </Nav.Brand>
+            <Nav.Brand>
+              <Link to='/login'>Login</Link>
+            </Nav.Brand>
+          </>
+        )}
+				</Nav>
+
+    </Nav>
+  )
 }
