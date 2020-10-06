71f5d7b03f2f03a4eeda632eb6b8d8a37f49f3fc
Habid B Espinosa <hab.bad@hotmail.com>
2020-10-06 17:07:08 +0200

    new routs for Pictures last year and new component

--- client/src/App.js
+++ client/src/App.js
@@ -5,72 +5,51 @@ import ContactInfo from "./components/ContactInfo";
 import ImageCar from "./components/ImageCar";
 import IntroApp from "./components/IntroApp";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Redirect, Switch } from 'react-router-dom';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import PictureInfo from './components/PictureInfo'
-import PictureOfTheDay from './components/PictureOfTheDay'
-
+import { Route, Redirect, Switch } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import PictureInfo from "./components/PictureInfo";
+import PictureOfTheDay from "./components/PictureOfTheDay";
+import PicturesLastYear from "./components/PicturesLastYear";
 
 class App extends Component {
-
   state = {
-    user: this.props.user
-  }
+    user: this.props.user,
+  };
 
-  setUser = user => {
+  setUser = (user) => {
     this.setState({
-      user: user
+      user: user,
     });
-  }
+  };
 
+  render() {
+    return (
+      <div className="App">
+        <Navbar user={this.state.user} setUser={this.setUser} />
 
-	render() {
-    
-		return (
-				<div className='App'>
-          <Navbar user={this.state.user} setUser={this.setUser} />
+        <Route exact path="/pictureOfTheDay" component={PictureOfTheDay} />
 
-          <Route 
-            exact
-            path='/pictureOfTheDay'
-            component={ PictureOfTheDay }
-          />
+        <Route exact path="/picturesLastYear" component={PicturesLastYear} />
 
-          
-					{/* <Route 
+        <Route exact path="/" component={ImageCar} />
+
+        <Route exact path="/:date" component={PictureInfo} />
+
+        <Route
           exact
-          path='/create' 
-          component={IntroApp} />  */}
-          
-          
-          <Route 
-            exact 
-            path="/" 
-            component={ ImageCar }  
-          />
-          
-          <Route 
-              exact
-              path='/:date'
-              component={ PictureInfo }
-            />
+          path="/signup"
+          render={(props) => <Signup setUser={this.setUser} {...props} />}
+        />
 
-            <Route
-              exact
-              path='/signup'
-              render={props => <Signup setUser={this.setUser} {...props} />}
-            />
-
-              <Route
-              exact
-              path='/login'
-              render={props => <Login setUser={this.setUser} {...props} />}
-            />
-				</div>
-		);
-	}
+        <Route
+          exact
+          path="/login"
+          render={(props) => <Login setUser={this.setUser} {...props} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
-
