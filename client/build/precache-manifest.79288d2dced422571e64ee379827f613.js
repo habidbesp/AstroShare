48bf8e30cd1965a82529d9a8d59f20529ef45e00
Habid B Espinosa <hab.bad@hotmail.com>
2020-10-08 19:29:09 +0200

    deployte

---
+++ client/build/precache-manifest.79288d2dced422571e64ee379827f613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3831e55f057631c71f76ffc16f1ec7af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7203280ebff9eaceb849",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b98f6a532a4df6129217",
+    "url": "/static/css/main.a8372852.chunk.css"
+  },
+  {
+    "revision": "7203280ebff9eaceb849",
+    "url": "/static/js/2.2e49b51a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2e49b51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98f6a532a4df6129217",
+    "url": "/static/js/main.6b99db86.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
